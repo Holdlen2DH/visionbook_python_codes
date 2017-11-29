63caa81b53216136ab5781026dcfb59c07699289
Holdlen2DH <donghaowhu@gmail.com>
2017-11-29 22:46:43 +0800

    read image with Pillow

--- 05Preproc/imsharpen_demo.py
+++ 05Preproc/imsharpen_demo.py
@@ -3,8 +3,20 @@ demo of imashapen Image sharping.
 
 by Holdlen2DH 2017-11-29
 """
-
+import os
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
 
 step = 1
 x1 = np.arange(1, 101, 1)
+
+img = Image.open(current_dir + "/images/patterns.png")
+print(img.format, img.size, img.mode)
+
+img_data = np.asarray(img, dtype = np.uint8)
+
+plt.imshow(img_data)
+plt.show()
