a4ae980c49dc9e46551e329b7f029eab09a4d5e4
Holdlen2DH <donghaowhu@gmail.com>
2018-01-04 15:03:07 +0800

    completed the dbpoundary_demo.py

--- 06Segm1/dpboundary_demo.py
+++ 06Segm1/dpboundary_demo.py
@@ -33,43 +33,63 @@ def dpboundary(IM):
     # or (y - 1, x - 1), respectively.
     M, N = IM.shape
     c = np.zeros((M, N))
-    p = np.zeors((M, N), type = 'uint8')        # save memory by using 8bit integers
+    p = np.zeros((M, N), dtype = 'uint8')        # save memory by using 8bit integers
     c[0, :] = IM[0, :]
 
     # the first pass of the algorithm goes through the image matrix c from the first
     # to the last row. For each row of c, we assemble a matrix d; each row of d corresponds
     # to one alternative (no shift, left shift, right shift) and contains the cost of reaching
-    # the current row of c.
-    for i in range(1, M + 1, 1):
+    # the current row of c. Note how the optimal choice is found in parallel for thw whole 
+    # row using the vectorized min function.
+    for i in range(1, M, 1):
         c0 = c[i - 1, :]
        
+        
         e1 = c0
         e2 = np.hstack((c0[1:], c0[-1]))
         e3 = np.hstack((c0[0], c0[0:-1]))
 
         e = np.vstack((e1, e2, e3))
-        d = np.tile(c0, (3, 1)) + e 
+        d = np.tile(IM[i - 1, :], (3, 1)) + e 
 
+        c[i, :] = np.min(d, axis = 0)
+        p[i, :] = np.argmin(d, axis = 0)
+
+    # The second part of the algorithm follows the optimal path from 'cheapest' node xpos in 
+    # the last row back to the first row, using the information from p and creating x on the way.
+    # We take care not to leave the image boundaries.
+
+    x = np.zeros((M, 1))
+    cost = np.min(c[M - 1, :])
+    xpos = np.argmin(c[M  - 1, :])
+
+    for i in range(M - 1, 0, -1):
+        x[i, 0] = xpos
+        if (p[i, xpos] == 1 and xpos < N - 1):
+            xpos = xpos + 1
+        elif p[i, xpos] == 2 and xpos > 0:
+            xpos = xpos - 1
+    
+    x[0, 0] = xpos
     return x
-
-c = np.arange(15).reshape(3, 5)
-c0 = c[1, :]
-print(c0)
-e1 = c0
-e2 = np.hstack((c0[1:], c0[-2]))
-print(e2)
-print(c0[0:-2])
-
 img = Image.open(current_dir + "/images/limb_vessels2.jpg")
 print(img.format, img.size, img.mode)
 
 img_data = np.asarray(img, dtype = np.uint8)
-
 fig = plt.figure()
 plt.subplot(1, 2, 1)
 plt.imshow(img_data, cmap = "gray")
 plt.title("Original image")
-plt.savefig(output_dir + "/dpboundary_output.jpg")
+
+x = dpboundary(-img_data.astype(np.float))
+
+plt.subplot(1, 2, 2)
+plt.imshow(img_data, cmap = "gray")
+plt.plot(x.flatten(), np.arange(0, img_data.shape[0], 1), 'r-')
+plt.title("tracing results")
+plt.suptitle("blood vessel tracing via dynamic programming")
+plt.savefig(output_dir + "/dpboundary_results.jpg")
+plt.show()
 
 
 
